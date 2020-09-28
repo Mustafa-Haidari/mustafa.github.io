ec1981741a29e8feb121a4b2fa3735860d4c6c9d
Mustafa Haidari <mhaidarpoor@gmail.com>
2020-09-29 01:54:28 +1000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,3 +210,14 @@
 		}
 
 })(jQuery);
+
+// visits counter
+const count = document.querySelector("#count");
+
+updateVisitCount();
+
+function updateVisitCount(){
+    fetch("https://api.countapi.xyz/update/countingVisits/MussPortfolio/?amount=1").then(res => res.json()).then(res => {
+        count.innerHTML = res.value;
+    })
+}
